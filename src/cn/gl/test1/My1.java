ccfc21a8dfceed02f7d049125cd974163e23f20a
test <test@qq.com>
2017-06-29 11:26:19 +0800

    2:第二次测试提交

--- src/cn/gl/test1/My1.java
+++ src/cn/gl/test1/My1.java
@@ -2,6 +2,7 @@ package cn.gl.test1;
 
 public abstract class My1 {
 	public  String a;
+	private int b;
 	public static void main(String args[]){
 	int a = 46;
 	String photos = "fingerprints\\123\\qw.bin";
