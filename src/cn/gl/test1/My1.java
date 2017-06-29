c26af3c660d0b32d55bdc17a986590c08d0cc972
test <test@qq.com>
2017-06-29 11:46:08 +0800

    4:测试

--- src/cn/gl/test1/My1.java
+++ src/cn/gl/test1/My1.java
@@ -4,6 +4,7 @@ public abstract class My1 {
 	public  String a;
 	private int b;
 	private float price;
+	
 	public static void main(String args[]){
 	int a = 46;
 	String photos = "fingerprints\\123\\qw.bin";
